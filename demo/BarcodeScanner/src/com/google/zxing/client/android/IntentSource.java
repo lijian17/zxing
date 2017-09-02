9e02215fb195699069be48fc6f7a991236aa6c79
lijian17 <374452668@qq.com>
2017-09-02 18:53:50 +0800

    意图来源

--- demo/BarcodeScanner/src/com/google/zxing/client/android/IntentSource.java
+++ demo/BarcodeScanner/src/com/google/zxing/client/android/IntentSource.java
@@ -1,26 +1,20 @@
-/*
- * Copyright (C) 2011 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.zxing.client.android;
 
+/**
+ * 意图来源
+ * 
+ * @author lijian
+ * @date 2017-9-2 下午6:53:15
+ */
 enum IntentSource {
 
-  NATIVE_APP_INTENT,
-  PRODUCT_SEARCH_LINK,
-  ZXING_LINK,
-  NONE
+	/** 本地APP意图 */
+	NATIVE_APP_INTENT,
+	/** 产品搜索链接 */
+	PRODUCT_SEARCH_LINK,
+	/** ZXing链接 */
+	ZXING_LINK,
+	/** 无 */
+	NONE
 
 }
