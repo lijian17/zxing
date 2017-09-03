fca7556e78421256696a2f03419c33d00eba0829
lijian17 <374452668@qq.com>
2017-09-03 19:32:54 +0800

    书签选择器

--- demo/BarcodeScanner/src/com/google/zxing/client/android/share/BookmarkPickerActivity.java
+++ demo/BarcodeScanner/src/com/google/zxing/client/android/share/BookmarkPickerActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2008 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.zxing.client.android.share;
 
 import java.util.ArrayList;
@@ -28,56 +12,59 @@ import android.view.View;
 import android.widget.ListView;
 
 /**
- * This class is only needed because I can't successfully send an ACTION_PICK intent to
- * com.android.browser.BrowserBookmarksPage. It can go away if that starts working in the future.
- *
- * @author dswitkin@google.com (Daniel Switkin)
+ * 此类仅需要，因为我无法成功发送ACTION_PICK意图到com.android.browser.BrowserBookmarksPage。
+ * 如果将来开始工作，它可能会消失。
+ * 
+ * @author lijian
+ * @date 2017-9-3 下午7:08:01
  */
 public final class BookmarkPickerActivity extends ListActivity {
+	private static final String TAG = BookmarkPickerActivity.class
+			.getSimpleName();
 
-  private static final String TAG = BookmarkPickerActivity.class.getSimpleName();
+	/** 书签投影 */
+	private static final String[] BOOKMARK_PROJECTION = {//
+	"title", // Browser.BookmarkColumns.TITLE
+			"url", // Browser.BookmarkColumns.URL
+	};
+	// 复制来自android.provider.Browser.BOOKMARKS_URI:
+	private static final Uri BOOKMARKS_URI = Uri
+			.parse("content://browser/bookmarks");
 
-  private static final String[] BOOKMARK_PROJECTION = {
-      "title", // Browser.BookmarkColumns.TITLE
-      "url", // Browser.BookmarkColumns.URL
-  };
-  // Copied from android.provider.Browser.BOOKMARKS_URI:
-  private static final Uri BOOKMARKS_URI = Uri.parse("content://browser/bookmarks");
+	private static final String BOOKMARK_SELECTION = "bookmark = 1 AND url IS NOT NULL";
 
-  private static final String BOOKMARK_SELECTION = "bookmark = 1 AND url IS NOT NULL";
+	private final List<String[]> titleURLs = new ArrayList<String[]>();
 
-  private final List<String[]> titleURLs = new ArrayList<String[]>();
+	@Override
+	protected void onResume() {
+		super.onResume();
+		titleURLs.clear();
+		Cursor cursor = getContentResolver().query(BOOKMARKS_URI,
+				BOOKMARK_PROJECTION, BOOKMARK_SELECTION, null, null);
+		if (cursor == null) {
+			Log.w(TAG, "没有返回书签查询的游标");
+			finish();
+			return;
+		}
+		try {
+			while (cursor.moveToNext()) {
+				titleURLs.add(new String[] { cursor.getString(0),
+						cursor.getString(1) });
+			}
+		} finally {
+			cursor.close();
+		}
+		setListAdapter(new BookmarkAdapter(this, titleURLs));
+	}
 
-  @Override
-  protected void onResume() {
-    super.onResume();
-    titleURLs.clear();
-    Cursor cursor = getContentResolver().query(BOOKMARKS_URI, BOOKMARK_PROJECTION,
-        BOOKMARK_SELECTION, null, null);
-    if (cursor == null) {
-      Log.w(TAG, "No cursor returned for bookmark query");
-      finish();
-      return;
-    }
-    try {
-      while (cursor.moveToNext()) {
-        titleURLs.add(new String[] { cursor.getString(0), cursor.getString(1) });
-      }
-    } finally {
-      cursor.close();
-    }
-    setListAdapter(new BookmarkAdapter(this, titleURLs));
-  }
-
-
-  @Override
-  protected void onListItemClick(ListView l, View view, int position, long id) {
-    String[] titleURL = titleURLs.get(position);
-    Intent intent = new Intent();
-    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-    intent.putExtra("title", titleURL[0]); // Browser.BookmarkColumns.TITLE
-    intent.putExtra("url", titleURL[1]); // Browser.BookmarkColumns.URL
-    setResult(RESULT_OK, intent);
-    finish();
-  }
+	@Override
+	protected void onListItemClick(ListView l, View view, int position, long id) {
+		String[] titleURL = titleURLs.get(position);
+		Intent intent = new Intent();
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+		intent.putExtra("title", titleURL[0]); // Browser.BookmarkColumns.TITLE
+		intent.putExtra("url", titleURL[1]); // Browser.BookmarkColumns.URL
+		setResult(RESULT_OK, intent);
+		finish();
+	}
 }
