67617310cf4f6a7db89c0d0f0100210fa42bdb56
lijian17 <374452668@qq.com>
2017-09-03 19:54:44 +0800

    选择已安装应用程序以通过Intent共享的活动

--- demo/BarcodeScanner/src/com/google/zxing/client/android/share/AppPickerActivity.java
+++ demo/BarcodeScanner/src/com/google/zxing/client/android/share/AppPickerActivity.java
@@ -1,20 +1,6 @@
-/*
- * Copyright (C) 2009 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.zxing.client.android.share;
+
+import java.util.List;
 
 import android.app.ListActivity;
 import android.content.Intent;
@@ -23,45 +9,47 @@ import android.view.View;
 import android.widget.Adapter;
 import android.widget.ListView;
 
-import java.util.List;
-
 /**
- * Activity for picking an installed application to share via Intent.
+ * 选择已安装应用程序以通过Intent共享的活动
+ * 
+ * @author lijian
+ * @date 2017-9-3 下午7:34:07
  */
 public final class AppPickerActivity extends ListActivity {
 
-  private AsyncTask<Object,Object,List<AppInfo>> backgroundTask;
+	private AsyncTask<Object, Object, List<AppInfo>> backgroundTask;
 
-  @Override
-  protected void onResume() {
-    super.onResume();
-    backgroundTask = new LoadPackagesAsyncTask(this);
-    backgroundTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-  }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		backgroundTask = new LoadPackagesAsyncTask(this);
+		backgroundTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+	}
 
-  @Override
-  protected void onPause() {
-    AsyncTask<?,?,?> task = backgroundTask;
-    if (task != null) {
-      task.cancel(true);
-      backgroundTask = null;
-    }
-    super.onPause();
-  }
+	@Override
+	protected void onPause() {
+		AsyncTask<?, ?, ?> task = backgroundTask;
+		if (task != null) {
+			task.cancel(true);
+			backgroundTask = null;
+		}
+		super.onPause();
+	}
 
-  @Override
-  protected void onListItemClick(ListView l, View view, int position, long id) {
-    Adapter adapter = getListAdapter();
-    if (position >= 0 && position < adapter.getCount()) {
-      String packageName = ((AppInfo) adapter.getItem(position)).getPackageName();
-      Intent intent = new Intent();
-      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
-      intent.putExtra("url", "market://details?id=" + packageName); // Browser.BookmarkColumns.URL
-      setResult(RESULT_OK, intent);
-    } else {
-      setResult(RESULT_CANCELED);      
-    }
-    finish();
-  }
+	@Override
+	protected void onListItemClick(ListView l, View view, int position, long id) {
+		Adapter adapter = getListAdapter();
+		if (position >= 0 && position < adapter.getCount()) {
+			String packageName = ((AppInfo) adapter.getItem(position))
+					.getPackageName();
+			Intent intent = new Intent();
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+			intent.putExtra("url", "market://details?id=" + packageName); // Browser.BookmarkColumns.URL
+			setResult(RESULT_OK, intent);
+		} else {
+			setResult(RESULT_CANCELED);
+		}
+		finish();
+	}
 
 }
