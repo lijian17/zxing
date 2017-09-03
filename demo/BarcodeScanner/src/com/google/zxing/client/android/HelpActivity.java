746e4579e88f5b90aa4561b12fe5806ccf68b5d0
lijian17 <374452668@qq.com>
2017-09-03 21:35:29 +0800

    帮助

--- demo/BarcodeScanner/src/com/google/zxing/client/android/HelpActivity.java
+++ demo/BarcodeScanner/src/com/google/zxing/client/android/HelpActivity.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2008 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.zxing.client.android;
 
 import android.app.Activity;
@@ -23,9 +7,10 @@ import android.webkit.WebView;
 import com.google.zxing.client.android.R;
 
 /**
- * An HTML-based help screen.
- *
- * @author dswitkin@google.com (Daniel Switkin)
+ * 基于HTML的帮助屏幕。
+ * 
+ * @author lijian
+ * @date 2017-9-3 下午9:24:54
  */
 public final class HelpActivity extends Activity {
 
