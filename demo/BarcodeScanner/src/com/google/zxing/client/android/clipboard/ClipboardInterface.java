6a6f5f7682267f3345a06086f0b03744518e4b2f
lijian17 <374452668@qq.com>
2017-09-03 20:07:55 +0800

    剪贴板

--- demo/BarcodeScanner/src/com/google/zxing/client/android/clipboard/ClipboardInterface.java
+++ demo/BarcodeScanner/src/com/google/zxing/client/android/clipboard/ClipboardInterface.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2012 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.zxing.client.android.clipboard;
 
 import android.content.ClipData;
@@ -22,43 +6,73 @@ import android.content.Context;
 import android.util.Log;
 
 /**
- * Abstraction over the {@link ClipboardManager} API that manages copying and pasting.
+ * 剪贴板<br>
+ * 抽象在管理复制和粘贴的{@link ClipboardManager} API
+ * 
+ * @author lijian
+ * @date 2017-9-3 下午7:55:54
  */
 public final class ClipboardInterface {
-  
-  private static final String TAG = ClipboardInterface.class.getSimpleName();
+	private static final String TAG = ClipboardInterface.class.getSimpleName();
 
-  private ClipboardInterface() {
-  }
+	private ClipboardInterface() {
+	}
 
-  public static CharSequence getText(Context context) {
-    ClipboardManager clipboard = getManager(context);
-    ClipData clip = clipboard.getPrimaryClip();
-    return hasText(context) ? clip.getItemAt(0).coerceToText(context) : null;
-  }
+	/**
+	 * 从剪贴板获取文本
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static CharSequence getText(Context context) {
+		ClipboardManager clipboard = getManager(context);
+		ClipData clip = clipboard.getPrimaryClip();
+		return hasText(context) ? clip.getItemAt(0).coerceToText(context)
+				: null;
+	}
 
-  public static void setText(CharSequence text, Context context) {
-    if (text != null) {
-      try {
-        getManager(context).setPrimaryClip(ClipData.newPlainText(null, text));
-      } catch (NullPointerException e){
-    	  // Have seen this in the wild, bizarrely
-    	  Log.w(TAG, "Clipboard bug", e);
-      } catch (IllegalStateException e) {
-        // Have seen this in the wild, bizarrely
-        Log.w(TAG, "Clipboard bug", e);
-      }
-    }
-  }
+	/**
+	 * 设置文本内容至剪贴板
+	 * 
+	 * @param text
+	 * @param context
+	 */
+	public static void setText(CharSequence text, Context context) {
+		if (text != null) {
+			try {
+				getManager(context).setPrimaryClip(
+						ClipData.newPlainText(null, text));
+			} catch (NullPointerException e) {
+				// 在wild看到这个，很奇怪
+				Log.w(TAG, "剪贴板bug", e);
+			} catch (IllegalStateException e) {
+				// 在wild看到这个，很奇怪
+				Log.w(TAG, "剪贴板bug", e);
+			}
+		}
+	}
 
-  public static boolean hasText(Context context) {
-    ClipboardManager clipboard = getManager(context);
-    ClipData clip = clipboard.getPrimaryClip();
-    return clip != null && clip.getItemCount() > 0;
-  }
-  
-  private static ClipboardManager getManager(Context context) {
-    return (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
-  }
+	/**
+	 * 剪切板是否有内容
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static boolean hasText(Context context) {
+		ClipboardManager clipboard = getManager(context);
+		ClipData clip = clipboard.getPrimaryClip();
+		return clip != null && clip.getItemCount() > 0;
+	}
+
+	/**
+	 * 获得剪切板管理器
+	 * 
+	 * @param context
+	 * @return
+	 */
+	private static ClipboardManager getManager(Context context) {
+		return (ClipboardManager) context
+				.getSystemService(Context.CLIPBOARD_SERVICE);
+	}
 
 }
