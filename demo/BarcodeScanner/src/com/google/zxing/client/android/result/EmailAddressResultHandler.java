fd3263dd481d91f8a0abcb036e6e97af5aae9262
lijian17 <374452668@qq.com>
2017-09-07 22:41:33 +0800

    处理email地址

--- demo/BarcodeScanner/src/com/google/zxing/client/android/result/EmailAddressResultHandler.java
+++ demo/BarcodeScanner/src/com/google/zxing/client/android/result/EmailAddressResultHandler.java
@@ -1,71 +1,60 @@
-/*
- * Copyright (C) 2008 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.zxing.client.android.result;
+
+import android.app.Activity;
 
 import com.google.zxing.client.android.R;
 import com.google.zxing.client.result.EmailAddressParsedResult;
 import com.google.zxing.client.result.ParsedResult;
 
-import android.app.Activity;
-
 /**
- * Handles email addresses.
- *
- * @author dswitkin@google.com (Daniel Switkin)
+ * 处理email地址
+ * 
+ * @author lijian
+ * @date 2017-9-7 下午10:39:27
  */
 public final class EmailAddressResultHandler extends ResultHandler {
-  private static final int[] buttons = {
-      R.string.button_email,
-      R.string.button_add_contact
-  };
+	private static final int[] buttons = {//
+	R.string.button_email,// 发送 E-mail
+			R.string.button_add_contact // 添加联系人
+	};
 
-  public EmailAddressResultHandler(Activity activity, ParsedResult result) {
-    super(activity, result);
-  }
+	/**
+	 * 处理email地址
+	 * 
+	 * @param activity
+	 * @param result
+	 */
+	public EmailAddressResultHandler(Activity activity, ParsedResult result) {
+		super(activity, result);
+	}
 
-  @Override
-  public int getButtonCount() {
-    return buttons.length;
-  }
+	@Override
+	public int getButtonCount() {
+		return buttons.length;
+	}
 
-  @Override
-  public int getButtonText(int index) {
-    return buttons[index];
-  }
+	@Override
+	public int getButtonText(int index) {
+		return buttons[index];
+	}
 
-  @Override
-  public void handleButtonPress(int index) {
-    EmailAddressParsedResult emailResult = (EmailAddressParsedResult) getResult();
-    switch (index) {
-      case 0:
-        sendEmail(emailResult.getTos(),
-                  emailResult.getCCs(),
-                  emailResult.getBCCs(),
-                  emailResult.getSubject(),
-                  emailResult.getBody());
-        break;
-      case 1:
-        addEmailOnlyContact(emailResult.getTos(), null);
-        break;
-    }
-  }
+	@Override
+	public void handleButtonPress(int index) {
+		EmailAddressParsedResult emailResult = (EmailAddressParsedResult) getResult();
+		switch (index) {
+		case 0:
+			sendEmail(emailResult.getTos(), emailResult.getCCs(),
+					emailResult.getBCCs(), emailResult.getSubject(),
+					emailResult.getBody());
+			break;
+		case 1:
+			addEmailOnlyContact(emailResult.getTos(), null);
+			break;
+		}
+	}
 
-  @Override
-  public int getDisplayTitle() {
-    return R.string.result_email_address;
-  }
+	@Override
+	public int getDisplayTitle() {
+		return R.string.result_email_address;// 找到电子邮件地址
+	}
 }
