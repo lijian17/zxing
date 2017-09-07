a088af5d48552845b95265cdb3ef4ff6064d7b23
lijian17 <374452668@qq.com>
2017-09-07 22:51:20 +0800

    处理以QR码编码的日历条目

--- demo/BarcodeScanner/src/com/google/zxing/client/android/result/CalendarResultHandler.java
+++ demo/BarcodeScanner/src/com/google/zxing/client/android/result/CalendarResultHandler.java
@@ -1,186 +1,175 @@
-/*
- * Copyright (C) 2008 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.zxing.client.android.result;
-
-import android.content.ActivityNotFoundException;
-import android.util.Log;
-import com.google.zxing.client.android.R;
-import com.google.zxing.client.result.CalendarParsedResult;
-import com.google.zxing.client.result.ParsedResult;
-
-import android.app.Activity;
-import android.content.Intent;
 
 import java.text.DateFormat;
 import java.util.Date;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.zxing.client.android.R;
+import com.google.zxing.client.result.CalendarParsedResult;
+import com.google.zxing.client.result.ParsedResult;
+
 /**
- * Handles calendar entries encoded in QR Codes.
- *
- * @author dswitkin@google.com (Daniel Switkin)
- * @author Sean Owen
+ * 处理以QR码编码的日历条目
+ * 
+ * @author lijian
+ * @date 2017-9-7 下午10:45:21
  */
 public final class CalendarResultHandler extends ResultHandler {
+	private static final String TAG = CalendarResultHandler.class
+			.getSimpleName();
 
-  private static final String TAG = CalendarResultHandler.class.getSimpleName();
+	private static final int[] buttons = {//
+	R.string.button_add_calendar // 添加至日程表
+	};
 
-  private static final int[] buttons = {
-      R.string.button_add_calendar
-  };
+	/**
+	 * 处理以QR码编码的日历条目
+	 * 
+	 * @param activity
+	 * @param result
+	 */
+	public CalendarResultHandler(Activity activity, ParsedResult result) {
+		super(activity, result);
+	}
 
-  public CalendarResultHandler(Activity activity, ParsedResult result) {
-    super(activity, result);
-  }
+	@Override
+	public int getButtonCount() {
+		return buttons.length;
+	}
 
-  @Override
-  public int getButtonCount() {
-    return buttons.length;
-  }
+	@Override
+	public int getButtonText(int index) {
+		return buttons[index];
+	}
 
-  @Override
-  public int getButtonText(int index) {
-    return buttons[index];
-  }
+	@Override
+	public void handleButtonPress(int index) {
+		if (index == 0) {
+			CalendarParsedResult calendarResult = (CalendarParsedResult) getResult();
 
-  @Override
-  public void handleButtonPress(int index) {
-    if (index == 0) {
-      CalendarParsedResult calendarResult = (CalendarParsedResult) getResult();
+			String description = calendarResult.getDescription();
+			String organizer = calendarResult.getOrganizer();
+			if (organizer != null) { // 没有单独的意图键，放在描述中
+				if (description == null) {
+					description = organizer;
+				} else {
+					description = description + '\n' + organizer;
+				}
+			}
 
-      String description = calendarResult.getDescription();
-      String organizer = calendarResult.getOrganizer();
-      if (organizer != null) { // No separate Intent key, put in description
-        if (description == null) {
-          description = organizer;
-        } else {
-          description = description + '\n' + organizer;
-        }
-      }
+			addCalendarEvent(calendarResult.getSummary(),
+					calendarResult.getStart(), calendarResult.isStartAllDay(),
+					calendarResult.getEnd(), calendarResult.getLocation(),
+					description, calendarResult.getAttendees());
+		}
+	}
 
-      addCalendarEvent(calendarResult.getSummary(),
-                       calendarResult.getStart(),
-                       calendarResult.isStartAllDay(),
-                       calendarResult.getEnd(),
-                       calendarResult.getLocation(),
-                       description,
-                       calendarResult.getAttendees());
-    }
-  }
+	/**
+	 * 通过预先添加添加事件UI来发送创建新的日历事件的意图。 系统的旧版本有一个错误，事件标题将不会被填写
+	 * 
+	 * @param summary
+	 *            事件的描述
+	 * @param start
+	 *            开始时间
+	 * @param allDay
+	 *            如果为真，事件被认为是从开始时起始的一整天
+	 * @param end
+	 *            结束时间（可选）
+	 * @param location
+	 *            事件位置的文本描述
+	 * @param description
+	 *            事件本身的文本描述
+	 * @param attendees
+	 *            与会者邀请
+	 */
+	private void addCalendarEvent(String summary, Date start, boolean allDay,
+			Date end, String location, String description, String[] attendees) {
+		Intent intent = new Intent(Intent.ACTION_INSERT);
+		intent.setType("vnd.android.cursor.item/event");
+		long startMilliseconds = start.getTime();
+		intent.putExtra("beginTime", startMilliseconds);
+		if (allDay) {
+			intent.putExtra("allDay", true);
+		}
+		long endMilliseconds;
+		if (end == null) {
+			if (allDay) {
+				// + 1 day
+				endMilliseconds = startMilliseconds + 24 * 60 * 60 * 1000;
+			} else {
+				endMilliseconds = startMilliseconds;
+			}
+		} else {
+			endMilliseconds = end.getTime();
+		}
+		intent.putExtra("endTime", endMilliseconds);
+		intent.putExtra("title", summary);
+		intent.putExtra("eventLocation", location);
+		intent.putExtra("description", description);
+		if (attendees != null) {
+			intent.putExtra(Intent.EXTRA_EMAIL, attendees);
+			// 文档说这是一个String[]或逗号分隔的String，这是正确的?
+		}
 
-  /**
-   * Sends an intent to create a new calendar event by prepopulating the Add Event UI. Older
-   * versions of the system have a bug where the event title will not be filled out.
-   *
-   * @param summary A description of the event
-   * @param start   The start time
-   * @param allDay  if true, event is considered to be all day starting from start time
-   * @param end     The end time (optional)
-   * @param location a text description of the event location
-   * @param description a text description of the event itself
-   * @param attendees attendees to invite
-   */
-  private void addCalendarEvent(String summary,
-                                Date start,
-                                boolean allDay,
-                                Date end,
-                                String location,
-                                String description,
-                                String[] attendees) {
-    Intent intent = new Intent(Intent.ACTION_INSERT);
-    intent.setType("vnd.android.cursor.item/event");
-    long startMilliseconds = start.getTime();
-    intent.putExtra("beginTime", startMilliseconds);
-    if (allDay) {
-      intent.putExtra("allDay", true);
-    }
-    long endMilliseconds;
-    if (end == null) {
-      if (allDay) {
-        // + 1 day
-        endMilliseconds = startMilliseconds + 24 * 60 * 60 * 1000;
-      } else {
-        endMilliseconds = startMilliseconds;
-      }
-    } else {
-      endMilliseconds = end.getTime();
-    }
-    intent.putExtra("endTime", endMilliseconds);
-    intent.putExtra("title", summary);
-    intent.putExtra("eventLocation", location);
-    intent.putExtra("description", description);
-    if (attendees != null) {
-      intent.putExtra(Intent.EXTRA_EMAIL, attendees);
-      // Documentation says this is either a String[] or comma-separated String, which is right?
-    }
+		try {
+			// 首先手动进行
+			rawLaunchIntent(intent);
+		} catch (ActivityNotFoundException anfe) {
+			Log.w(TAG, "没有可以响应的日历应用程序 " + Intent.ACTION_INSERT);
+			// 对于不喜欢“INSERT”的日历应用程序：
+			intent.setAction(Intent.ACTION_EDIT);
+			launchIntent(intent); // 如果没有任何东西可以处理它，这里是真的
+		}
+	}
 
-    try {
-      // Do this manually at first
-      rawLaunchIntent(intent);
-    } catch (ActivityNotFoundException anfe) {
-      Log.w(TAG, "No calendar app available that responds to " + Intent.ACTION_INSERT);
-      // For calendar apps that don't like "INSERT":
-      intent.setAction(Intent.ACTION_EDIT);
-      launchIntent(intent); // Fail here for real if nothing can handle it
-    }
-  }
+	@Override
+	public CharSequence getDisplayContents() {
 
+		CalendarParsedResult calResult = (CalendarParsedResult) getResult();
+		StringBuilder result = new StringBuilder(100);
 
-  @Override
-  public CharSequence getDisplayContents() {
+		ParsedResult.maybeAppend(calResult.getSummary(), result);
 
-    CalendarParsedResult calResult = (CalendarParsedResult) getResult();
-    StringBuilder result = new StringBuilder(100);
+		Date start = calResult.getStart();
+		ParsedResult.maybeAppend(format(calResult.isStartAllDay(), start),
+				result);
 
-    ParsedResult.maybeAppend(calResult.getSummary(), result);
+		Date end = calResult.getEnd();
+		if (end != null) {
+			if (calResult.isEndAllDay() && !start.equals(end)) {
+				// 仅显示年/月/日
+				// 如果是全天，这是结束日期，它是排他的，所以显示用户
+				// 它在前一天结束，使更直观的感觉。
+				// 但是如果事件已经（不正确地）指定了相同的开始/结束，那么不要这样做
+				end = new Date(end.getTime() - 24 * 60 * 60 * 1000);
+			}
+			ParsedResult.maybeAppend(format(calResult.isEndAllDay(), end),
+					result);
+		}
 
-    Date start = calResult.getStart();
-    ParsedResult.maybeAppend(format(calResult.isStartAllDay(), start), result);
+		ParsedResult.maybeAppend(calResult.getLocation(), result);
+		ParsedResult.maybeAppend(calResult.getOrganizer(), result);
+		ParsedResult.maybeAppend(calResult.getAttendees(), result);
+		ParsedResult.maybeAppend(calResult.getDescription(), result);
+		return result.toString();
+	}
 
-    Date end = calResult.getEnd();
-    if (end != null) {
-      if (calResult.isEndAllDay() && !start.equals(end)) {
-        // Show only year/month/day
-        // if it's all-day and this is the end date, it's exclusive, so show the user
-        // that it ends on the day before to make more intuitive sense.
-        // But don't do it if the event already (incorrectly?) specifies the same start/end
-        end = new Date(end.getTime() - 24 * 60 * 60 * 1000);
-      }
-      ParsedResult.maybeAppend(format(calResult.isEndAllDay(), end), result);
-    }
+	private static String format(boolean allDay, Date date) {
+		if (date == null) {
+			return null;
+		}
+		DateFormat format = allDay ? DateFormat
+				.getDateInstance(DateFormat.MEDIUM) : DateFormat
+				.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
+		return format.format(date);
+	}
 
-    ParsedResult.maybeAppend(calResult.getLocation(), result);
-    ParsedResult.maybeAppend(calResult.getOrganizer(), result);
-    ParsedResult.maybeAppend(calResult.getAttendees(), result);
-    ParsedResult.maybeAppend(calResult.getDescription(), result);
-    return result.toString();
-  }
-
-  private static String format(boolean allDay, Date date) {
-    if (date == null) {
-      return null;
-    }
-    DateFormat format = allDay
-        ? DateFormat.getDateInstance(DateFormat.MEDIUM)
-        : DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM);
-    return format.format(date);
-  }
-
-  @Override
-  public int getDisplayTitle() {
-    return R.string.result_calendar;
-  }
+	@Override
+	public int getDisplayTitle() {
+		return R.string.result_calendar;// 找到日程
+	}
 }
