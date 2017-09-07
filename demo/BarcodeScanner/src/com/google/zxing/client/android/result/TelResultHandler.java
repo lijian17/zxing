98f38b70820ba026d9bf820f3af7f002ececb8d5
lijian17 <374452668@qq.com>
2017-09-07 22:12:25 +0800

    提供电话号码的相关行动

--- demo/BarcodeScanner/src/com/google/zxing/client/android/result/TelResultHandler.java
+++ demo/BarcodeScanner/src/com/google/zxing/client/android/result/TelResultHandler.java
@@ -1,82 +1,72 @@
-/*
- * Copyright (C) 2008 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.zxing.client.android.result;
+
+import android.app.Activity;
+import android.telephony.PhoneNumberUtils;
 
 import com.google.zxing.client.android.R;
 import com.google.zxing.client.result.ParsedResult;
 import com.google.zxing.client.result.TelParsedResult;
 
-import android.app.Activity;
-import android.telephony.PhoneNumberUtils;
-
 /**
- * Offers relevant actions for telephone numbers.
- *
- * @author dswitkin@google.com (Daniel Switkin)
+ * 提供电话号码的相关行动
+ * 
+ * @author lijian
+ * @date 2017-9-7 下午10:09:52
  */
 public final class TelResultHandler extends ResultHandler {
-  private static final int[] buttons = {
-      R.string.button_dial,
-      R.string.button_add_contact
-  };
+	private static final int[] buttons = {//
+	R.string.button_dial,// 拨打电话
+			R.string.button_add_contact // 添加联系人
+	};
 
-  public TelResultHandler(Activity activity, ParsedResult result) {
-    super(activity, result);
-  }
+	/**
+	 * 
+	 * 提供电话号码的相关行动
+	 * 
+	 * @param activity
+	 * @param result
+	 */
+	public TelResultHandler(Activity activity, ParsedResult result) {
+		super(activity, result);
+	}
 
-  @Override
-  public int getButtonCount() {
-    return buttons.length;
-  }
+	@Override
+	public int getButtonCount() {
+		return buttons.length;
+	}
 
-  @Override
-  public int getButtonText(int index) {
-    return buttons[index];
-  }
+	@Override
+	public int getButtonText(int index) {
+		return buttons[index];
+	}
 
-  @Override
-  public void handleButtonPress(int index) {
-    TelParsedResult telResult = (TelParsedResult) getResult();
-    switch (index) {
-      case 0:
-        dialPhoneFromUri(telResult.getTelURI());
-        // When dialer comes up, it allows underlying display activity to continue or something,
-        // but app can't get camera in this state. Avoid issues by just quitting, only in the
-        // case of a phone number
-        getActivity().finish();
-        break;
-      case 1:
-        String[] numbers = new String[1];
-        numbers[0] = telResult.getNumber();
-        addPhoneOnlyContact(numbers, null);
-        break;
-    }
-  }
+	@Override
+	public void handleButtonPress(int index) {
+		TelParsedResult telResult = (TelParsedResult) getResult();
+		switch (index) {
+		case 0:
+			dialPhoneFromUri(telResult.getTelURI());
+			// 当拨号器出现时，它允许基础显示活动继续或某事，但应用程序无法在此状态下获取相机。 只有在电话号码的情况下才可以避免出现问题
+			getActivity().finish();
+			break;
+		case 1:
+			String[] numbers = new String[1];
+			numbers[0] = telResult.getNumber();
+			addPhoneOnlyContact(numbers, null);
+			break;
+		}
+	}
 
-  // Overriden so we can take advantage of Android's phone number hyphenation routines.
-  @Override
-  public CharSequence getDisplayContents() {
-    String contents = getResult().getDisplayResult();
-    contents = contents.replace("\r", "");
-    return PhoneNumberUtils.formatNumber(contents);
-  }
+	// 被覆盖，所以我们可以利用Android的手机号连字例程
+	@Override
+	public CharSequence getDisplayContents() {
+		String contents = getResult().getDisplayResult();
+		contents = contents.replace("\r", "");
+		return PhoneNumberUtils.formatNumber(contents);
+	}
 
-  @Override
-  public int getDisplayTitle() {
-    return R.string.result_tel;
-  }
+	@Override
+	public int getDisplayTitle() {
+		return R.string.result_tel;// 找到电话号码
+	}
 }
