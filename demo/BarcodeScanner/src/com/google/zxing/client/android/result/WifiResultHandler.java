c536db2cb1361605a1397bc84204d75154b9cece
lijian17 <374452668@qq.com>
2017-09-07 21:59:05 +0800

    处理Wifi上网信息

--- demo/BarcodeScanner/src/com/google/zxing/client/android/result/WifiResultHandler.java
+++ demo/BarcodeScanner/src/com/google/zxing/client/android/result/WifiResultHandler.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2008 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.zxing.client.android.result;
 
 import android.app.Activity;
@@ -30,63 +14,75 @@ import com.google.zxing.client.result.ParsedResult;
 import com.google.zxing.client.result.WifiParsedResult;
 
 /**
- * Handles wifi access information.
- *
- * @author Vikram Aggarwal
- * @author Sean Owen
+ * 处理Wifi上网信息
+ * 
+ * @author lijian
+ * @date 2017-9-7 下午9:26:25
  */
 public final class WifiResultHandler extends ResultHandler {
+	private static final String TAG = WifiResultHandler.class.getSimpleName();
 
-  private static final String TAG = WifiResultHandler.class.getSimpleName();
+	private final CaptureActivity parent;
 
-  private final CaptureActivity parent;
+	/**
+	 * 
+	 * 处理Wifi上网信息
+	 * 
+	 * @param activity
+	 * @param result
+	 */
+	public WifiResultHandler(CaptureActivity activity, ParsedResult result) {
+		super(activity, result);
+		parent = activity;
+	}
 
-  public WifiResultHandler(CaptureActivity activity, ParsedResult result) {
-    super(activity, result);
-    parent = activity;
-  }
+	@Override
+	public int getButtonCount() {
+		// 我们只需要一个按钮，那就是配置无线。 这可能会在将来改变
+		return 1;
+	}
 
-  @Override
-  public int getButtonCount() {
-    // We just need one button, and that is to configure the wireless.  This could change in the future.
-    return 1;
-  }
+	@Override
+	public int getButtonText(int index) {
+		return R.string.button_wifi;// 连接到网络
+	}
 
-  @Override
-  public int getButtonText(int index) {
-    return R.string.button_wifi;
-  }
+	@Override
+	public void handleButtonPress(int index) {
+		if (index == 0) {
+			WifiParsedResult wifiResult = (WifiParsedResult) getResult();
+			WifiManager wifiManager = (WifiManager) getActivity()
+					.getSystemService(Context.WIFI_SERVICE);
+			if (wifiManager == null) {
+				Log.w(TAG, "没有可用的设备wifiManager");
+				return;
+			}
+			final Activity activity = getActivity();
+			activity.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					// 连接到 Wi-Fi...
+					Toast.makeText(activity.getApplicationContext(),
+							R.string.wifi_changing_network, Toast.LENGTH_SHORT)
+							.show();
+				}
+			});
+			new WifiConfigManager(wifiManager).executeOnExecutor(
+					AsyncTask.THREAD_POOL_EXECUTOR, wifiResult);
+			parent.restartPreviewAfterDelay(0L);
+		}
+	}
 
-  @Override
-  public void handleButtonPress(int index) {
-    if (index == 0) {
-      WifiParsedResult wifiResult = (WifiParsedResult) getResult();
-      WifiManager wifiManager = (WifiManager) getActivity().getSystemService(Context.WIFI_SERVICE);
-      if (wifiManager == null) {
-        Log.w(TAG, "No WifiManager available from device");
-        return;
-      }
-      final Activity activity = getActivity();
-      activity.runOnUiThread(new Runnable() {
-        @Override
-        public void run() {
-          Toast.makeText(activity.getApplicationContext(), R.string.wifi_changing_network, Toast.LENGTH_SHORT).show();
-        }
-      });
-      new WifiConfigManager(wifiManager).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, wifiResult);
-      parent.restartPreviewAfterDelay(0L);
-    }
-  }
+	// 向用户显示网络名称和网络类型
+	@Override
+	public CharSequence getDisplayContents() {
+		WifiParsedResult wifiResult = (WifiParsedResult) getResult();
+		return wifiResult.getSsid() + " (" + wifiResult.getNetworkEncryption()
+				+ ')';
+	}
 
-  // Display the name of the network and the network type to the user.
-  @Override
-  public CharSequence getDisplayContents() {
-    WifiParsedResult wifiResult = (WifiParsedResult) getResult();
-    return wifiResult.getSsid() + " (" + wifiResult.getNetworkEncryption() + ')';
-  }
-
-  @Override
-  public int getDisplayTitle() {
-    return R.string.result_wifi;
-  }
+	@Override
+	public int getDisplayTitle() {
+		return R.string.result_wifi;// 找到 Wi-Fi 配置
+	}
 }
