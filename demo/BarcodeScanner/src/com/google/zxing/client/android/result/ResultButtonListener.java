fc865904d4f61f0e7413f8f4b5152a5b2e9ba028
lijian17 <374452668@qq.com>
2017-09-07 22:21:47 +0800

    在Android平台的上下文中处理条形码解码的结果，通过发送正确的意图来打开其他活动，如GMail，Maps等

--- demo/BarcodeScanner/src/com/google/zxing/client/android/result/ResultButtonListener.java
+++ demo/BarcodeScanner/src/com/google/zxing/client/android/result/ResultButtonListener.java
@@ -1,40 +1,24 @@
-/*
- * Copyright (C) 2008 ZXing authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.zxing.client.android.result;
 
 import android.view.View;
 
 /**
- * Handles the result of barcode decoding in the context of the Android platform, by dispatching the
- * proper intents to open other activities like GMail, Maps, etc.
- *
- * @author dswitkin@google.com (Daniel Switkin)
+ * 在Android平台的上下文中处理条形码解码的结果，通过发送正确的意图来打开其他活动，如GMail，Maps等
+ * 
+ * @author lijian
+ * @date 2017-9-7 下午10:18:42
  */
 public final class ResultButtonListener implements View.OnClickListener {
-  private final ResultHandler resultHandler;
-  private final int index;
+	private final ResultHandler resultHandler;
+	private final int index;
 
-  public ResultButtonListener(ResultHandler resultHandler, int index) {
-    this.resultHandler = resultHandler;
-    this.index = index;
-  }
+	public ResultButtonListener(ResultHandler resultHandler, int index) {
+		this.resultHandler = resultHandler;
+		this.index = index;
+	}
 
-  @Override
-  public void onClick(View view) {
-    resultHandler.handleButtonPress(index);
-  }
+	@Override
+	public void onClick(View view) {
+		resultHandler.handleButtonPress(index);
+	}
 }
